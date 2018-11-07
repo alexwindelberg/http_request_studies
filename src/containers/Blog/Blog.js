18f737e7ed1df868a89d55b114832454a48c99c8
alexwindelberg <alexwindelberg@gmail.com>
2018-11-07 15:42:45 -0500

    added newpost and posts routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 // we will be using jsonplaceholder to request and get dummy data
 
@@ -31,8 +32,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1> Home </h1> } />
-                <Route path="/" exact render={() => <h1> Home 2 </h1> } />
+                { /* <Route path="/" exact render={() => <h1> Home </h1> } />
+                <Route path="/" exact render={() => <h1> Home 2 </h1> } /> */ }
+                {/* When routing this is how you render components based on the route that users wish to go */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
